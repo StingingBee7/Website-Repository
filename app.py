bd17795821ae6ea973e502b165de244b263a77df
hntrl <hunter@hntrl.io>
2024-10-08 15:32:54 -0600

    feat: resume upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import boto3
 from botocore.exceptions import NoCredentialsError
-import os
 
 S3_BUCKET = 'kylerfileupload' 
 
@@ -10,8 +9,21 @@ s3 = boto3.client('s3')
 app = Flask(__name__)
 
 @app.route('/')
-def upload_form():
-    return render_template('index.html')
+def index():
+	try:
+		response = s3.list_objects_v2(Bucket=S3_BUCKET)
+		files = [obj['Key'] for obj in response.get('Contents', [])]
+	except NoCredentialsError:
+		files = []
+
+	signed_urls = []
+	for file in files:
+		url = s3.generate_presigned_url('get_object',
+										Params={'Bucket': S3_BUCKET, 'Key': file},
+										ExpiresIn=3600)
+		signed_urls.append(url)
+
+	return render_template('index.html', files=signed_urls)
 
 # hunter's contribution :)
 a, b = 0, 1
@@ -35,7 +47,7 @@ def upload_file():
     if file:
         #Fill in later using S3 code
         s3.upload_fileobj(file, S3_BUCKET, file.filename, ExtraArgs={'ContentType':file.content_type})
-        return redirect('/success')
+        return redirect(f'/{file.filename}')
 
 @app.route('/success')
 def upload_success():
@@ -44,3 +56,13 @@ def upload_success():
 @app.route('/resume')
 def resume():
     return render_template('ClayResume.html')
+
+@app.route('/<filename>')
+def get_file(filename):
+    try:
+        file_obj = s3.get_object(Bucket=S3_BUCKET, Key=filename)
+        return file_obj['Body'].read()
+    except s3.exceptions.NoSuchKey:
+        return "File not found", 404
+    except Exception as e:
+        return f"An error occurred: {str(e)}", 500
